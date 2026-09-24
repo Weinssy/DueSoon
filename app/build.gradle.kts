56f7c306beac3a0215ba230dc10dcca0221ed615
Rifa Zam Zami <rifazamzami13@gmail.com>
2026-09-24 21:11:17 +0700

    chore: Bump version to 1.9.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.duesoon.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.9.1"
+        versionCode = 12
+        versionName = "1.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
